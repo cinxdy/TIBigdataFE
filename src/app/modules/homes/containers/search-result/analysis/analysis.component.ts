d9de23646409c9bcf0569fc4cefccf849b356c92
SongJinBeom <cooo8@naver.com>
2020-01-30 16:38:40 +0900

    pull B repo

--- src/app/modules/homes/containers/search-result/analysis/analysis.component.ts
+++ src/app/modules/homes/containers/search-result/analysis/analysis.component.ts
@@ -1,68 +1,62 @@
-import { Component, OnInit } from '@angular/core';
-import { ElasticsearchService } from '../../../service/elasticsearch.service';
-import * as CanvasJS from '../../../../../../assets/canvasjs.min.js';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { ElasticsearchService } from "../../../service/elasticsearch.service";
+import * as CanvasJS from "../../../../../../assets/canvasjs.min.js";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 @Component({
-	selector: 'app-analysis',
-	templateUrl: './analysis.component.html',
-	styleUrls: ['./analysis.component.less']
+  selector: "app-analysis",
+  templateUrl: "./analysis.component.html",
+  styleUrls: ["./analysis.component.less"]
 })
 export class AnalysisComponent implements OnInit {
+  private searchKeyword;
+  private TEST_URL: string = "http://localhost:5000/keywordGraph";
+  private BASE_URL: string = "http://203.252.103.123:5000/keywordGraph";
+  private currURL = this.BASE_URL;
 
-	private searchKeyword;
-	private TEST_URL: string = 'http://localhost:5000/keywordGraph';
-	private BASE_URL: string = 'http://203.252.103.123:5000/keywordGraph';
-	private currURL = this.TEST_URL;
-
-	private headers: HttpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
-	constructor(
-		private http: HttpClient,
-		private es: ElasticsearchService) { }
+  private headers: HttpHeaders = new HttpHeaders({
+    "Content-Type": "application/json"
+  });
+  constructor(private http: HttpClient, private es: ElasticsearchService) {}
 
 	ngOnInit() {
 		this.searchKeyword = this.es.getKeyword();
 		console.log(this.searchKeyword);
 
-		let body =
-			{ "keyword": this.searchKeyword }
+    let body = { keyword: this.searchKeyword };
 
-		this.http.post(this.currURL,
-			body, { headers: this.headers })
-			.subscribe(
-				(data) => {
+    this.http
+      .post(this.currURL, body, { headers: this.headers })
+      .subscribe(data => {
+        let chart = new CanvasJS.Chart("chartContainer", {
+          animationEnabled: true,
+          title: {
+            text: "키워드 [" + this.searchKeyword + "] 에 대한 자료 수"
+          },
+          axisY: {
+            title: "자료 수",
+            includeZero: false
+          },
+          toolTip: {
+            shared: true
+          },
+          data: [
+            {
+              type: "spline",
+              name: "전체 자료 수",
+              showInLegend: true,
+              dataPoints: data[0]
+            },
+            {
+              type: "spline",
+              name: "검색 자료 수",
+              showInLegend: true,
+              dataPoints: data[1]
+            }
+          ]
+        });
 
-					let chart = new CanvasJS.Chart("chartContainer", {
-						animationEnabled: true,
-						title: {
-							text: "키워드 [" + this.searchKeyword + "] 에 대한 자료 수"
-						},
-						axisY: {
-							title: "자료 수",
-							includeZero: false
-						},
-						toolTip: {
-							shared: true
-						},
-						data: [{
-							type: "spline",
-							name: "전체 자료 수",
-							showInLegend: true,
-							dataPoints: data[0]
-						},
-						{
-							type: "spline",
-							name: "검색 자료 수",
-							showInLegend: true,
-							dataPoints: data[1]
-						}]
-					});
-
-					chart.render();
-				}
-			)
-
-
-	}
-
+        chart.render();
+      });
+  }
 }
