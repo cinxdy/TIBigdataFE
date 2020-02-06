f324d1b9cc213e29f730911a1e9bb6876a94fa81
BaekTree <etknbe@gmail.com>
2020-02-06 20:28:50 +0900

    child component

--- src/app/modules/homes/body/search/search-routing.module.ts
+++ src/app/modules/homes/body/search/search-routing.module.ts
@@ -1,32 +1,32 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { SearchResultComponent } from './search-result/search-result.component';
-import { FreqAnalysisComponent } from './freq-analysis/freq-analysis.component';
-import { ChosenDocAnalysisComponent } from './chosen-doc-analysis/chosen-doc-analysis.component';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { SearchResultComponent } from "./search-result/search-result.component";
+import { FreqAnalysisComponent } from "./freq-analysis/freq-analysis.component";
+import { ChosenDocAnalysisComponent } from "./chosen-doc-analysis/chosen-doc-analysis.component";
 
 const routes: Routes = [
   {
-    path : "",
-    component : SearchResultComponent
+    path: "",
+    component: SearchResultComponent
   },
   {
-    path : "homes/searchResult",
-    component : SearchResultComponent
-  },
-  {
-    path : "homes/searchResult/freqAnalysis",
-    component : FreqAnalysisComponent
-  },
-  {
-    path : "homes/searchResult/ChosenDocAnalysis",
-    component : ChosenDocAnalysisComponent
+    path: "homes/searchResult",
+    component: SearchResultComponent,
+    children: [
+      {
+        path: "freqAnalysis",
+        component: FreqAnalysisComponent
+      },
+      {
+        path: "ChosenDocAnalysis",
+        component: ChosenDocAnalysisComponent
+      }
+    ]
   }
-  
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class SearchRoutingModule { }
+export class SearchRoutingModule {}
