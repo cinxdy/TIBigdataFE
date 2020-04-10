2e5b41ed9ed0b4dc15c1d931b461563f3448d36e
BaekTree <etknbe@gmail.com>
2020-04-10 20:55:21 +0900

    fail ver

--- UniCenter_backend/server.js
+++ UniCenter_backend/server.js
@@ -3,26 +3,25 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
-const mongoose = require('mongoose'); //mongose 서버와 백엔드 연결 
 
 const app = express();
 
 const api = require('./api');//bring the backend func and feature
 
 const PORT = 4000;
-const db = 'mongodb://localhost:27017';
+// const db = 'mongodb://localhost:27017';
+
+// //connect to db
+// mongoose.connect(db, err => {
+//     if (err) {
+//         console.error('Error!' + err)
+//     } else {
+//         console.log('Connected to mongodb');
+//     }
+// });
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', api);//api 경로에서 항상 require("./api") 호출한다. use : middleware 함수.
-
-//connect to db
-mongoose.connect(db, err => {
-    if (err) {
-        console.error('Error!' + err)
-    } else {
-        console.log('Connected to mongodb');
-    }
-});
 
 //root dir
 app.get('/', function(req, res) {
