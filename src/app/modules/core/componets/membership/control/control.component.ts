3735aaaf7222678fb3749434bc699b1f10eccc0f
BaekTree <etknbe@gmail.com>
2020-04-28 14:44:04 +0900

    in while fail token update

--- src/app/modules/core/componets/membership/control/control.component.ts
+++ src/app/modules/core/componets/membership/control/control.component.ts
@@ -14,29 +14,56 @@ export class ControlComponent implements OnInit {
   private userEmailReqURL: string = this.ipservice.getCommonIp() + ":4000/??";
   private userGoogleReqUrl: string = this.ipservice.getCommonIp() + ""
   private HISTORY_URL: string = this.ipservice.getCommonIp() + ":4000/hst/getTotalHistory";
+  private HISTORY_COUNT_URL: string = this.ipservice.getCommonIp() + ":4000/hst/getHistoryCount";
+
   private E_USER_URL: string = this.ipservice.getCommonIp() + ":4000/eUser/getEuserList";
   private G_USER_URL: string = this.ipservice.getCommonIp() + ":4000/gUser/getGuserList";
+
+  private pageNum : number;
 
   // private isSudo : booelean;
   private users: any[] = [];
   private hst: any[] = [];
 
-  ngOnInit() {
-    this.http.get<any>(this.HISTORY_URL).subscribe((res) => {
-      var r = res.histories;
-      for (var i = 0; i < r.length; i++) {
-        var d = r[i];
-        this.hst.push(
-          {
-            key: d.keyword,
-            time: d.year + "년" + d.month + "월" + d.date + "일" + d.hour + "시" + d.min + "분"
-          });
-      }
-    });
+  private HST_PER_PAGE : number = 50;
 
+  ngOnInit() {
+
+    //get history count
+    this.http.get<any>(this.HISTORY_COUNT_URL).subscribe((res) => {
+      let count = res;
+      this.pageNum = count / this.HST_PER_PAGE;//total pages.
+      let startIdx = 0;
+      var num = this.HST_PER_PAGE;
+      // while(1){
+      //   if(startIdx > count){
+      //     //show left over
+      //     num = startIdx - count;
+      //     break;
+      //   }
+
+        //get total history
+        this.http.post<any>(this.HISTORY_URL,{idx : startIdx, num : num}).subscribe((res) => {
+          var r = res.histories;
+          for (var i = 0; i < r.length; i++) {
+            var d = r[i];
+            this.hst.push(
+              {
+                key: d.keyword,
+                time: d.year + "년" + d.month + "월" + d.date + "일" + d.hour + "시" + d.min + "분"
+              });
+          }
+        });
+
+      //   startIdx += this.HST_PER_PAGE;
+      // }
+    })
+
+
+    //get email user info
     this.http.get<any>(this.E_USER_URL).subscribe((res) => {
       // console.log(r);
-      if(!res.succ){
+      if (!res.succ) {
         console.error("http request error in getting all user list in control component")
       }
       var r = res.payload;
@@ -46,16 +73,17 @@ export class ControlComponent implements OnInit {
         this.users.push(
           {
             email: d.email,
-            inst:d.inst,
-            name:d.name,
-            nickname:d.nickname,
-            password:d.password
+            inst: d.inst,
+            name: d.name,
+            nickname: d.nickname,
+            password: d.password
           });
       }
 
     });
+
+
+
+
   }
-
-
-
 }
