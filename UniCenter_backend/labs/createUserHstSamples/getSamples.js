22281a4dc3727ab1287a3c2d51504ac8104cfd75
baekTree <etknbe@gmail.com>
2020-06-15 17:38:57 +0900

    update

--- UniCenter_backend/labs/createUserHstSamples/getSamples.js
+++ UniCenter_backend/labs/createUserHstSamples/getSamples.js
@@ -5,39 +5,39 @@ const fs = require('fs');
 
 const hst = require('../../models/history');
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
 
-const app = express();
-const mongoose = require('mongoose'); //mongose 서버와 백엔드 연결 
-
-
-const PORT = 4000;
-
-const db = 'mongodb://localhost:27017/user';
+// const app = express();
+// const mongoose = require('mongoose'); //mongose 서버와 백엔드 연결 
 
 
-mongoose.connect(db, err => {
-    if (err) {
-        console.error('Error!' + err)
-    } else {
-        console.log('Connected to mongodb');
-    }
-});
+// const PORT = 4000;
 
-app.use(cors());
-app.use(bodyParser.json());
+// const db = 'mongodb://localhost:27017/user';
 
-//root dir
-app.get('/', function (req, res) {
-    res.send('Hello from server');
-})
 
-//server listen with no time interval?
-app.listen(PORT, function () {
-    console.log('Express server running on port ' + PORT)
-});
+// mongoose.connect(db, err => {
+//     if (err) {
+//         console.error('Error!' + err)
+//     } else {
+//         console.log('Connected to mongodb');
+//     }
+// });
+
+// app.use(cors());
+// app.use(bodyParser.json());
+
+// //root dir
+// app.get('/', function (req, res) {
+//     res.send('Hello from server');
+// })
+
+// //server listen with no time interval?
+// app.listen(PORT, function () {
+//     console.log('Express server running on port ' + PORT)
+// });
 
 py.stdout.on('data', (d) => {
     var rawdata = fs.readFileSync(pyDir + '/tokened_history.json');
