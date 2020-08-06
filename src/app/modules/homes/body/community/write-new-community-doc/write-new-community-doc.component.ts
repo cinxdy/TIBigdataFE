9dc7b43e62e3f8dfde1f1d341f3af72ef9c61b89
baekTree <etknbe@gmail.com>
2020-08-06 14:30:25 +0900

    update

--- src/app/modules/homes/body/community/write-new-community-doc/write-new-community-doc.component.ts
+++ src/app/modules/homes/body/community/write-new-community-doc/write-new-community-doc.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { Router } from "@angular/router";
 import { CommunityServiceService } from 'src/app/modules/communications/fe-backend-db/community/community-service.service';
 import { EPAuthService } from '../../../../communications/fe-backend-db/membership/auth.service';
@@ -8,11 +8,33 @@ import { EPAuthService } from '../../../../communications/fe-backend-db/membersh
   templateUrl: './write-new-community-doc.component.html',
   styleUrls: ['./write-new-community-doc.component.less']
 })
-export class WriteNewCommunityDocComponent implements OnInit {
+export class WriteNewCommunityDocComponent implements AfterViewInit {
+  @ViewChild('result',{static: false}) result: ElementRef;
+  @ViewChild('tbString',{static: false}) tbString: ElementRef;
+  @ViewChild('fontColor',{static: false}) fontColor: ElementRef;
+  @ViewChild('fontSize',{static: false}) fontSize: ElementRef;
+  @ViewChild('fontOptions',{static: false}) fontOptions: ElementRef;
+  @ViewChild('pRef', {static: false}) pRef: ElementRef;
+
+      // var objResult = document.getElementById("result");
+  
+    // var tbString = document.getElementById("tbString");
+  
+    // var fontColor = document.getElementById("fontColor");
+  
+    // var fontSize = document.getElementById("fontSize");
+  
+    // var fontOptions = document.getElementsByName("fontOptions");
 
   constructor( private router: Router, private cm_svs : CommunityServiceService, private auth : EPAuthService) { }
+
+    ngAfterViewInit() {
+    console.log(this.pRef.nativeElement.innerHTML); 
+    this.pRef.nativeElement.innerHTML = "DOM updated successfully!!!"; 
+  }
   private title : string;
   private content : string;
+  private fondOptions : boolean[] = [];
   
   ngOnInit() {
     
@@ -38,6 +60,100 @@ export class WriteNewCommunityDocComponent implements OnInit {
     this.toCommunity();
 
   }
+
+  previewString(){
+ 
+    // var objResult = document.getElementById("result");
+  
+    // var tbString = document.getElementById("tbString");
+  
+    // var fontColor = document.getElementById("fontColor");
+  
+    // var fontSize = document.getElementById("fontSize");
+  
+    // var fontOptions = document.getElementsByName("fontOptions");
+  
+  
+  
+  
+  
+  
+  
+    //tag id name 이3개를 읽을수 있음
+  
+  
+    console.log(this.tbString, this.fontColor, this.fontOptions);
+  
+    var targetString = this.tbString.nativeElement.value; 
+  
+    targetString = targetString.fontcolor(this.fontColor.nativeElement.options[this.fontColor.nativeElement.selectedIndex].value);
+  
+       targetString = targetString.fontsize(this.fontSize.nativeElement.options[this.fontSize.nativeElement.selectedIndex].value);
+  
+       
+  
+    if(this.fondOptions[0]){
+  
+     targetString = targetString.strike();
+  
+    }
+  
+    if(this.fondOptions[1]){
+  
+     targetString = targetString.big();
+  
+    }
+  
+    if(this.fondOptions[2]){
+  
+     targetString = targetString.small();
+  
+    }
+  
+    if(this.fondOptions[3]){
+  
+     targetString = targetString.bold();
+  
+    }
+  
+    if(this.fondOptions[4]){
+  
+     targetString = targetString.italic();
+  
+    }
+  
+    if(this.fondOptions[5]){
+  
+     targetString = targetString.sup();
+  
+    }
+  
+    if(this.fondOptions[6]){
+  
+     targetString = targetString.sub();
+  
+    }
+  
+    if(this.fondOptions[7]){
+  
+     targetString = targetString.toLowerCase();
+  
+    }
+  
+    if(this.fondOptions[8]){
+  
+     targetString = targetString.toUpperCase();
+  
+    }
+  
+     this.result.nativeElement.innerHTML = targetString;
+  
+   
+  
+  
+  
+  
+   }
 
   updateContent($event){
     this.content = $event.target.value;
